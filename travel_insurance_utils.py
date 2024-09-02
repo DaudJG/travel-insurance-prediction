911fcdfd52223cd90aa527fbc09db4dfd0e3b74f
DaudJG <daudfey@gmail.com>
2024-09-02 22:43:45 +0200

    create utility file and imported necessary libraries

---
+++ travel_insurance_utils.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple, Dict
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+from scipy.stats import chi2_contingency, mannwhitneyu
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
